b49e08e37f538d6f916964952a2f23e41c6da732
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2021-10-16 22:23:31 -0300

    Adiciona script

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const number1 = 20
+const number2 = 12
+
+
+function soma() {
+    return soma = number1 + number2
+}
+
+function sub() {
+    return sub = number1 - number2
+}
+
+function divisor() {
+    return divisor = number1 / number2
+}
+
+function mutiplicar() {
+    return mutiplicar = number1 * number2
+}
+
